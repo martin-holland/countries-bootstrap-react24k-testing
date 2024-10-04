3b67eb31025039d4ccf2befac2ae92f07bb44c6f
Martin Holland <martin.holland@free.fi>
2024-10-04 13:40:37 +0300

    end of jest testing

--- src/__tests__/countriesApiTest.spec.js
+++ src/__tests__/countriesApiTest.spec.js
@@ -1,19 +1,21 @@
-import { getAllCountries } from "../services/countriesServices";
+// import { getAllCountries } from "../services/countriesServices";
 
-describe("Countries API should return data", () => {
-  //Check that the data is an array (since the API returns an array of countries)
+// describe("Countries API should return data", () => {
+//   //Check that the data is an array (since the API returns an array of countries)
+//   let data;
+//   beforeAll(async () => {
+//     data = await getAllCountries();
+//   });
 
-  it("API should return an array of countries", async () => {
-    const data = await getAllCountries();
-    expect(Array.isArray(data)).toBe(true);
-  });
+//   it("API should return an array of countries", async () => {
+//     expect(Array.isArray(data)).toBe(true);
+//   });
 
-  it("Each item should contain the following properties", async () => {
-    const data = await getAllCountries();
-    data.forEach((country) => {
-      expect(country).toHaveProperty("name");
-      expect(country).toHaveProperty("area");
-      expect(country).toHaveProperty("population");
-    });
-  });
-});
+//   it("Each item should contain the following properties", async () => {
+//     data.forEach((country) => {
+//       expect(country).toHaveProperty("name");
+//       expect(country).toHaveProperty("area");
+//       expect(country).toHaveProperty("population");
+//     });
+//   });
+// });
